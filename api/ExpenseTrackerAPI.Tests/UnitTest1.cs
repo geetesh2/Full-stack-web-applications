93147cdde51b314adf2056844a176b86b6001100
Geetesh <geeteshpandey2@gmail.com>
2025-11-08 10:18:46 +0530

    Add test project and ci step

---
+++ api/ExpenseTrackerAPI.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace ExpenseTrackerAPI.Tests
+{
+    public class MathTests
+    {
+        [Fact]
+        public void Addition_Works()
+        {
+            int a = 2;
+            int b = 3;
+            int result = a + b;
+            Assert.Equal(5, result);
+        }
+    }
+}
